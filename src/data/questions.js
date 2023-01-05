6c7f6a2ec1c1e15a497fa513e8b76603c91a624f
Elizabeth Hollinger <111613075+EHollingerFllStk@users.noreply.github.com>
2023-01-05 16:03:50 -0500

    Initial commit

---
+++ src/data/questions.js
@@ -0,0 +1,94 @@
+const questions = [
+  {
+    title: "What was Prince's name at birth?",
+    options: [
+      {
+        description: "Roger Nelson"
+      },
+      {
+        description: "Christopher Tracey"
+      },
+      {
+        description: "Prince Rogers Nelson"
+      },
+      {
+        description: "Prince"
+      }
+    ],
+    correctAnswer: 3
+  },
+  {
+    title: "Who designed the Prince Symbol?",
+    options: [
+      {
+        description: "Prince"
+      },
+      {
+        description: "Wendy Melvoin"
+      },
+      {
+        description: "Mitch Monson"
+      },
+      {
+        description: "Gayle Chapman"
+      }
+    ],
+    correctAnswer: 3
+  },
+  {
+    title: "Which of the following is used in pencils",
+    options: [
+      {
+        description: "When Doves Cry"
+      },
+      {
+        description: "If I Was Your Girlfriend"
+      },
+      {
+        description: "Purple Rain"
+      },
+      {
+        description: "Sign O'The Times"
+      }
+    ],
+    correctAnswer: 1
+  },
+  {
+    title: "How many Grammy's does Prince have?",
+    options: [
+      {
+        description: "1"
+      },
+      {
+        description: "7"
+      },
+      {
+        description: "3"
+      },
+      {
+        description: "9"
+      }
+    ],
+    correctAnswer: 2
+  },
+  {
+    title: "What organizations has Prince donated to?",
+    options: [
+      {
+        description: "Elton John AIDS Foundation"
+      },
+      {
+        description: "People For Ethical Treatment of Animals"
+      },
+      {
+        description: "The Bridge for Urban Youth and Farming"
+      },
+      {
+        description: "All of the Above"
+      }
+    ],
+    correctAnswer: 4
+  }
+];
+
+export default questions;
