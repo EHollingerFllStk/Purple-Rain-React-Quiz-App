4085bd52ff2f0147be4f293f33de10711cfe7189
Elizabeth Hollinger <hollingere15@gmail.com>
2023-01-12 00:20:35 +0000

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -3,92 +3,110 @@ const questions = [
     title: "What was Prince's name at birth?",
     options: [
       {
-        description: "Roger Nelson"
+        description: "Roger Nelson",
       },
       {
-        description: "Christopher Tracey"
+        description: "Christopher Tracey",
       },
       {
-        description: "Prince Rogers Nelson"
+        description: "Prince Rogers Nelson",
       },
       {
-        description: "Prince"
-      }
+        description: "Prince",
+      },
     ],
-    correctAnswer: 3
+    correctAnswer: 3,
   },
   {
     title: "Who designed the Prince Symbol?",
     options: [
       {
-        description: "Prince"
+        description: "Prince",
       },
       {
-        description: "Wendy Melvoin"
+        description: "Wendy Melvoin",
       },
       {
-        description: "Mitch Monson"
+        description: "Mitch Monson",
       },
       {
-        description: "Gayle Chapman"
-      }
+        description: "Gayle Chapman",
+      },
     ],
-    correctAnswer: 3
+    correctAnswer: 3,
   },
   {
-    title: "Which of the following is used in pencils",
+    title: "Which song is Prince's #1 song?",
     options: [
       {
-        description: "When Doves Cry"
+        description: "When Doves Cry",
       },
       {
-        description: "If I Was Your Girlfriend"
+        description: "If I Was Your Girlfriend",
       },
       {
-        description: "Purple Rain"
+        description: "Purple Rain",
       },
       {
-        description: "Sign O'The Times"
-      }
+        description: "Sign O'The Times",
+      },
     ],
-    correctAnswer: 1
+    correctAnswer: 1,
   },
   {
     title: "How many Grammy's does Prince have?",
     options: [
       {
-        description: "1"
+        description: "1",
       },
       {
-        description: "7"
+        description: "7",
       },
       {
-        description: "3"
+        description: "3",
       },
       {
-        description: "9"
-      }
+        description: "9",
+      },
     ],
-    correctAnswer: 2
+    correctAnswer: 2,
   },
   {
     title: "What organizations has Prince donated to?",
     options: [
       {
-        description: "Elton John AIDS Foundation"
+        description: "Elton John AIDS Foundation",
       },
       {
-        description: "People For Ethical Treatment of Animals"
+        description: "People For Ethical Treatment of Animals",
       },
       {
-        description: "The Bridge for Urban Youth and Farming"
+        description: "The Bridge for Urban Youth and Farming",
       },
       {
-        description: "All of the Above"
-      }
+        description: "All of the Above",
+      },
     ],
-    correctAnswer: 4
-  }
+    correctAnswer: 4,
+  },
+  {
+    title: "What is Prince's father's name?",
+    options: [
+      {
+        description: "Prince Sr.",
+      },
+      {
+        description: "John L Nelson",
+      },
+      {
+        description: "Prince Rogers Nelson Sr.",
+      },
+      {
+        description: "Richard J Nelson",
+      },
+    ],
+    correctAnswer: 2,
+  },
 ];
 
 export default questions;
